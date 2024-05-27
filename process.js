b2c8531ae5dcdb52310809f5b16fbcf6734ab55c
Sandip Datta <sandip.ddb@gmail.com>
2024-05-27 16:53:36 +0000

    updates

--- process.js
+++ process.js
@@ -8,9 +8,12 @@ async function initPyodide() {
     await pyodide.loadPackage('micropip');
 
     // Install the local PyMuPDF wheel
+    // CHANGED -->  await micropip.install('./wheels/PyMuPDF-1.24.4-cp311-none-emscripten_3_1_32_wasm32.whl')
+    // TO --> await micropip.install('./wheels/PyMuPDF-1.24.4-cp311-none-emscripten_3_1_46_wasm32.whl')
+    // ELSE Error - "Wheel was built with Emscripten v3.1.32 but Pyodide was built with Emscripten v3.1.46"
     await pyodide.runPythonAsync(`
         import micropip
-        await micropip.install('./wheels/PyMuPDF-1.24.4-cp311-none-emscripten_3_1_32_wasm32.whl')
+        await micropip.install('./wheels/PyMuPDF-1.24.4-cp311-none-emscripten_3_1_46_wasm32.whl')
     `);
 
     document.getElementById('runDemo').addEventListener('click', async () => {
