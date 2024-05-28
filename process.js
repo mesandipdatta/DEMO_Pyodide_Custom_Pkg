b826ec173c465600f43d8f0f62e5a522e293fb03
Sandip Datta <sandip.ddb@gmail.com>
2024-05-28 09:49:42 +0000

    loadPyodide

--- process.js
+++ process.js
@@ -1,29 +1,14 @@
-async function initPyodide() {
-    const pyodide = await loadPyodide({
-        //indexURL: './pyodide/'
-        indexURL: "https://cdn.jsdelivr.net/pyodide/v0.25.0/full/"
-    });
+import { loadPyodide } from 'pyodide';
 
-    // Load the micropip package
-    await pyodide.loadPackage('micropip');
+async function main() {
+    // Load Pyodide with the custom stdlib URL
+    const pyodide = await loadPyodide({ stdLibURL: "./python_stdlib_patched.zip" });
 
-    // Install the local PyMuPDF wheel
-    // CHANGED -->  await micropip.install('./wheels/PyMuPDF-1.24.4-cp311-none-emscripten_3_1_32_wasm32.whl')
-    // TO --> await micropip.install('./wheels/PyMuPDF-1.24.4-cp311-none-emscripten_3_1_46_wasm32.whl')
-    // ELSE Error - "Wheel was built with Emscripten v3.1.32 but Pyodide was built with Emscripten v3.1.46"
-    await pyodide.runPythonAsync(`
-        import micropip
-        await micropip.install('./wheels/PyMuPDF-1.24.4-cp311-none-emscripten_3_1_46_wasm32.whl')
+    // Run Python code using the bundled packages
+    pyodide.runPython(`
+        import mymodule.run
+        mymodule.run.myfunc()
     `);
-
-    document.getElementById('runDemo').addEventListener('click', async () => {
-        const result = await pyodide.runPythonAsync(`
-            import main
-            main.main()
-        `);
-
-        document.getElementById('output').innerText = result;
-    });
 }
 
-initPyodide();
+main();
