4609dd423b8b7df1537e0aa343991480c06de09e
Sandip Datta <sandip.ddb@gmail.com>
2024-05-30 12:50:22 +0000

    Updates

--- main1.py
+++
@@ -1,5 +0,0 @@
-def translate_english(text):
-    # Replace this function with the actual translation logic
-    # pirate_speak = text.replace("hello", "ahoy").replace("my", "me")
-    pirate_speak = "Hello from Python"
-    return pirate_speak
