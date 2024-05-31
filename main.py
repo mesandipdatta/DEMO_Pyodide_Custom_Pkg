862c6be3b7486b0db926504533861e869aa12d55
Sandip Datta <sandip.ddb@gmail.com>
2024-05-31 09:39:09 +0000

    pymupdf_path

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 def translate_english(text):
     # Replace this function with the actual translation logic
     # pirate_speak = text.replace("hello", "ahoy").replace("my", "me")
+    import_libs()
     pirate_speak = "Hello from Python"
     return pirate_speak
+
+def import_libs():
+    import sys
+    import os
+
+    # Get the directory where the current script is located
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Construct paths to the desired subdirectories
+    pymupdf_path = os.path.join(script_dir, 'py-pkgs', 'pymupdf')
+    #sm1_path = os.path.join(script_dir, 'py_pkgs', 'sm1')
+
+    # Append both paths to the end of sys.path
+    sys.path.append(pymupdf_path)
+    #sys.path.append(sm1_path)
+
+    # Now you can import your dependencies
+    import fitz  # pymupdf
